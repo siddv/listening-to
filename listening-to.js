4a6baf8fd575cdf3843bc515059638305d8fa061
Siddharth Vadgama <siddv@hotmail.com>
2016-05-07 23:21:34 +0100

    adds function to render html to the page, along with styles

--- listening-to.js
+++ listening-to.js
@@ -1,15 +1,15 @@
 /**
  * ListeningTo constructor class
- * @param user
- * @param APIKey
+ * @param config {object}
  * @returns {ListeningTo}
  * @constructor
  */
-function ListeningTo(user, APIKey){
+function ListeningTo(config) {
 
     var self = this;
-    this.user = user;
-    this.APIKey = APIKey;
+    this.user = config.user;
+    this.APIKey = config.APIKey;
+    this.theme = config.theme ? 'listening-to--' + config.theme : '';
 
     /**
      * simple http GET request
@@ -19,7 +19,7 @@ function ListeningTo(user, APIKey){
      * @returns {Promise}
      */
     function httpGet(url, resolve, reject) {
-        return new Promise(function() {
+        return new Promise(function () {
 
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange = function () {
@@ -40,28 +40,49 @@ function ListeningTo(user, APIKey){
      */
     this.getLatestSong = function (resolve, reject) {
         var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' +
-                self.user +
-                '&api_key=' +
-                self.APIKey +
-                '&format=json';
+            self.user +
+            '&api_key=' +
+            self.APIKey +
+            '&format=json';
 
         return httpGet(
             url,
-            function(songs) {
+            function (songs) {
                 songs = JSON.parse(songs);
 
                 var latestSong = songs.recenttracks.track[0];
                 var song = {};
 
-                song.name = latestSong.name;
+                song.title = latestSong.name;
                 song.artist = latestSong.artist['#text'];
+                song.link = latestSong.url;
                 song.listening = latestSong['@attr'] && latestSong['@attr'].nowplaying == true ? true : false;
 
                 resolve(song);
             },
-            function(err) {
+            function (err) {
                 reject(err);
             }
         );
     };
+
+
+    /**
+     * Renders the widget on the page
+     */
+    this.show = function () {
+        self.getLatestSong(function (song) {
+
+            var listening = song.listening ? 'Currently listening to: ' : 'Last listened to: '
+
+            document.body.innerHTML +=
+                '<div class="listening-to ' + self.theme + '">' +
+                '<a href="http://www.last.fm/user/'+ self.user + '" target="_blank" class="listening-to__listening">' + listening + '</a>' +
+                '<a href="' + song.link + '" target="_blank" class="listening-to__song">' + song.title + '</a>' +
+                '<div class="listening-to__artist">' + song.artist + '</div>' +
+                '</div>';
+        });
+    };
+
+    return this;
 }
