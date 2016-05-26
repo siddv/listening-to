aa76445a18f1ab4ea92906bffcf3a28e3c1dfaf8
Siddharth Vadgama <siddv@hotmail.com>
2016-05-26 08:25:07 +0100

    Implements native JS promises properly

--- listening-to.js
+++ listening-to.js
@@ -11,15 +11,14 @@ function ListeningTo(config) {
     this.APIKey = config.APIKey;
     this.theme = config.theme ? 'listening-to--' + config.theme : '';
 
+
     /**
      * simple http GET request
      * @param url
-     * @param resolve
-     * @param reject
      * @returns {Promise}
      */
-    function httpGet(url, resolve, reject) {
-        return new Promise(function () {
+    function httpGet(url) {
+        return new Promise(function (resolve, reject) {
 
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange = function () {
@@ -34,35 +33,32 @@ function ListeningTo(config) {
 
     /**
      * gets last played song
-     * @param resolve
-     * @param reject
      * @returns {Promise}
      */
-    this.getLatestSong = function (resolve, reject) {
+    this.getLatestSong = function () {
         var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' +
             self.user +
             '&api_key=' +
             self.APIKey +
             '&format=json';
 
-        return httpGet(
-            url,
-            function (songs) {
-                songs = JSON.parse(songs);
+        return httpGet(url)
+            .then(function (songs) {
+                    songs = JSON.parse(songs);
 
-                var latestSong = songs.recenttracks.track[0];
+                    var latestSong = songs.recenttracks.track[0];
 
-                resolve({
-                    title: latestSong.name,
-                    artist: latestSong.artist['#text'],
-                    link: latestSong.url,
-                    listening: latestSong['@attr'] && latestSong['@attr'].nowplaying == 'true' ? true : false
-                });
-            },
-            function (err) {
-                reject(err);
-            }
-        );
+                    return {
+                        title: latestSong.name,
+                        artist: latestSong.artist['#text'],
+                        link: latestSong.url,
+                        listening: latestSong['@attr'] && latestSong['@attr'].nowplaying == 'true' ? true : false
+                    };
+                },
+                function (err) {
+                    console.error('Could not get lastest song:', err);
+                }
+            );
     };
 
 
@@ -70,18 +66,18 @@ function ListeningTo(config) {
      * Renders the widget on the page
      */
     this.show = function () {
-        self.getLatestSong(function (song) {
 
-            var listening = song.listening ? 'Currently listening to: ' : 'Last listened to: ';
+        self.getLatestSong()
+            .then(function (song) {
 
-            document.body.innerHTML +=
-                '<div class="listening-to ' + self.theme + '">' +
-                '<a href="http://www.last.fm/user/' + self.user + '" target="_blank" class="listening-to__listening">' + listening + '</a>' +
-                '<a href="' + song.link + '" target="_blank" class="listening-to__song">' + song.title + '</a>' +
-                '<div class="listening-to__artist">' + song.artist + '</div>' +
-                '</div>';
-        });
+                var listening = song.listening ? 'Currently listening to: ' : 'Last listened to: ';
+
+                document.body.innerHTML +=
+                    '<div class="listening-to ' + self.theme + '">' +
+                    '<a href="http://www.last.fm/user/' + self.user + '" target="_blank" class="listening-to__listening">' + listening + '</a>' +
+                    '<a href="' + song.link + '" target="_blank" class="listening-to__song">' + song.title + '</a>' +
+                    '<div class="listening-to__artist">' + song.artist + '</div>' +
+                    '</div>';
+            });
     };
-
-    return this;
 }
